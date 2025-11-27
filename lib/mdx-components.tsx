5fb6da12ec3228105efe1404efcb1a83c1b70ad2
Sanuli Gehara <geharasanuli5@gmail.com>
2025-11-27 15:45:02 +0530

    Add get-started content

--- lib/mdx-components.tsx
+++ lib/mdx-components.tsx
@@ -1,12 +1,15 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import { Step, Steps } from 'fumadocs-ui/components/steps';
 import type { MDXComponents } from 'mdx/types';
+import { Card, Cards } from 'fumadocs-ui/components/card';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     Step,
     Steps,
+    Card,
+    Cards,
     ...components,
   };
 }
